cdf4d3e4309ec80a8d10ac0c609e0dc6060b2ea1
phamducthien4325 <23020160@vnu.edu.>
2025-05-23 09:10:16 +0700

    Enhance forward difference functions with detailed docstrings and update test values

--- chuong6.1: tinh gan dung dao ham/forward_difference.py
+++ chuong6.1: tinh gan dung dao ham/forward_difference.py
@@ -8,24 +8,61 @@ import math
 # |____________________________________________|
 # neu h < 0 thi goi la backward difference
 def forward_difference(f, x0: float, h: float) -> float:
+    """
+    :param f: function to be derived
+    :param x0: point to be derived
+    :param h: step size
+    :return: derivative of f at x0
+    """
     return (f(x0 + h) - f(x0)) / h
     
 def forward_difference_2(fx0: float, fxh: float, x0: float, h: float) -> float:
+    """
+    :param fx0: f(x0)
+    :param fxh: f(x0 + h)
+    :param x0: point to be derived
+    :param h: step size
+    :return: derivative of f at x0
+    """
     return (fxh - fx0) / h
 
 def fx(x):
-    return math.sin(x)
+    """
+    :param x: point to be derived
+    :return: f(x)
+    """
+    return x ** 2 * math.log(x) + 1
 
 def fprime(x):
-    return math.cos(x)
+    """
+    :param x: point to be derived
+    :return: f'(x)
+    """
+    return 2 * x * math.log(x) + 1
 
 def abs_f2prime(x):
-    return math.fabs(-math.sin(x))
+    """
+    :param x: point to be derived
+    :return: |f''(x)|
+    """
+    return abs(2 * math.log(x) + 3)
 
 def error(f, x0: float, fx0: float) -> float:
+    """
+    :param f: function to be derived
+    :param x0: point to be derived
+    :param fx0: f(x0)
+    :return: error of f'(x0)
+    """
     return abs(f(x0) - fx0)
 
 def error_bound(f2prime, a: float, b: float) -> float:
+    """
+    :param f2prime: second derivative of f
+    :param a: left bound
+    :param b: right bound
+    :return: error bound of f'(x0)
+    """
     if a > b:
         a, b = b, a
     res_min = minimize_scalar(f2prime, bounds=(a, b), method='bounded')
@@ -33,10 +70,10 @@ def error_bound(f2prime, a: float, b: float) -> float:
     return res_min.fun, res_max.fun
 
 if __name__ == '__main__':
-    x0 = 0.0
-    fx0 = 0
+    x0 = 1.2
+    fx0 = 1.2625
     h = 0.2
-    fxh = 0.7414
+    fxh = 1.6595
     f_prime = forward_difference_2(fx0, fxh, x0, h)
     print(f"f'({x0}) = {f_prime:.4f}")
     print(f"Error: {error(fprime, 0.5, f_prime):.4f}")
