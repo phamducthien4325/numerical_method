f414e178d7a9c9e4d401d479027791391f619703
phamducthien4325 <23020160@vnu.edu.>
2025-05-23 08:47:05 +0700

    Refactor forward difference implementation and add error handling functions

--- chuong6.1: tinh gan dung dao ham/forward_difference.py
+++ chuong6.1: tinh gan dung dao ham/forward_difference.py
@@ -1,3 +1,6 @@
+from scipy.optimize import minimize_scalar
+import math
+
 #  ____________________________________________
 # |          f(x0 + h) - f(x0)     h           |
 # |f'(x0) = ------------------- - --- * f''(ξ) |
@@ -11,12 +14,33 @@ def forward_difference_2(fx0: float, fxh: float, x0: float, h: float) -> float:
     return (fxh - fx0) / h
 
 def fx(x):
-    return x ** 2 + 3 * x + 2
+    return math.sin(x)
+
+def fprime(x):
+    return math.cos(x)
+
+def abs_f2prime(x):
+    return math.fabs(-math.sin(x))
+
+def error(f, x0: float, fx0: float) -> float:
+    return abs(f(x0) - fx0)
+
+def error_bound(f2prime, a: float, b: float) -> float:
+    if a > b:
+        a, b = b, a
+    res_min = minimize_scalar(f2prime, bounds=(a, b), method='bounded')
+    res_max = minimize_scalar(lambda x: -f2prime(x), bounds=(a, b), method='bounded')
+    return res_min.fun, res_max.fun
 
 if __name__ == '__main__':
-    x0 = 0.6
-    fx0 = 0.5646
-    h = -0.1
-    fxh = 0.4794
+    x0 = 0.0
+    fx0 = 0
+    h = 0.2
+    fxh = 0.7414
     f_prime = forward_difference_2(fx0, fxh, x0, h)
     print(f"f'({x0}) = {f_prime:.4f}")
+    print(f"Error: {error(fprime, 0.5, f_prime):.4f}")
+    a, b = error_bound(abs_f2prime, x0, x0 + h)
+    a = abs(h / 2 * a)
+    b = abs(h / 2 * b)
+    print(f"Error bound: [{a:.4f}, {b:.4f}]")
