fd0f5c95c3c28c385d81c740ddb71115f3d40b13
phamducthien4325 <23020160@vnu.edu.vn>
2025-05-25 19:46:47 +0700

    Refactor find_k function for cubic spline; update parameter names and enhance matrix setup for second derivatives calculation.

--- chuong4: noi suy ham so/spline.py
+++ chuong4: noi suy ham so/spline.py
@@ -5,7 +5,7 @@ sys.path.append(str(Path(__file__).parent.parent))
 import numpy as np
 from chuong3 import gauss
 
-def find_k(fi: list[float], k0: float, k4: float, h: float) -> list[float]:
+def find_k(fi: list[float], k0: float, kn: float, h: float) -> list[float]:
     """
     :param fi: list of function values at xi
     :param k0: value of the first derivative at the first point
@@ -13,16 +13,28 @@ def find_k(fi: list[float], k0: float, k4: float, h: float) -> list[float]:
     :param h: step size (assumed constant)
     :return: list of second derivatives at xi
     """
-    A = np.array([
-        [4, 1, 0],
-        [1, 4, 1],
-        [0, 1, 4],
-    ], dtype=float)
-    b = 3 / h * np.array([[fi[2] - fi[0]],
-                          [fi[3] - fi[1]],
-                          [fi[4] - fi[2]]], dtype=float) - np.array([[k0], [0], [k4]], dtype=float)
-    k = [k0, 0, 0, 0, k4]
-    k[1:4] = gauss(A, b).flatten().tolist()
+    n = len(fi) - 1
+    A = np.zeros((n - 1, n - 1), dtype=float)
+    b = np.zeros((n - 1, 1), dtype=float)
+    k = [0] * (n + 1)
+    for i in range(0, n - 1):
+        if i == 0:
+            A[0][0] = 4
+            A[0][1] = 1
+        elif i == n - 2:
+            A[i][i - 1] = 1
+            A[i][i] = 4
+        else:
+            A[i][i - 1] = 1
+            A[i][i] = 4
+            A[i][i + 1] = 1
+    for i in range(0, n - 1):
+        b[i] = 3 / h * (fi[i + 2] - fi[i])
+    b[0] -= k0
+    b[n - 2] -= kn
+    k[1:n] = gauss(A, b).flatten().tolist()
+    k[0] = k0
+    k[n] = kn
     return k
 
 def cubic_spline(fi: list[float], k: list[float], h: float):
@@ -32,8 +44,9 @@ def cubic_spline(fi: list[float], k: list[float], h: float):
     :param h: step size (assumed constant)
     :return: list of cubic spline coefficients
     """
-    a = [[0] * 4 for _ in range(4)]
-    for j in range(4):
+    n = len(fi) - 1
+    a = [[0] * 4 for _ in range(n)]
+    for j in range(n):
         a[j][0] = fi[j]
         a[j][1] = k[j]
         a[j][2] = 3 / h**2 * (fi[j + 1] - fi[j]) - (2 * k[j] + k[j + 1]) / h
@@ -42,9 +55,10 @@ def cubic_spline(fi: list[float], k: list[float], h: float):
 
 if __name__ == '__main__':
     k0 = 0
-    kn = 0
-    xi = [-2, -1, 0, 1, 2]
-    fi = [0, 0, 1, 0, 0]
+    kn = -12
+    xi = [0, 2, 4, 6]
+    fi = [1, 9, 41, 41]  
+    n = len(xi) - 1
     k = find_k(fi, k0, kn, xi[1] - xi[0])
     a = cubic_spline(fi, k, xi[1] - xi[0])
     print('Cac he so cua spline cubic la:')
