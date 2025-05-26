17e0db3ac69980425a7c3c0b68de29cc8cad46dc
phamducthien4325 <23020160@vnu.edu.vn>
2025-05-26 09:38:59 +0700

    Add Heun's method implementation for solving ODEs; update main function to print correct method name.

--- chuong7: phuong trinh vi phan/heun.py
+++ chuong7: phuong trinh vi phan/heun.py
@@ -1,15 +1,14 @@
 import sympy as sp
 import math
 
-def mid_point(x0: float, y0: float, h: float, xn: float, y_prime, x, y) -> list[tuple[float, float]]:
+def heun(x0: float, y0: float, h: float, xn: float, y_prime, x, y) -> list[tuple[float, float]]:
     xi = x0
     yi = y0
     result = [(xi, yi)]
     
     while xi < xn:
-        y1_2 = yi + h / 2 * y_prime.subs({x: xi, y: yi}).evalf()
-        x1_2 = xi + h / 2
-        yi += h * y_prime.subs({x: x1_2, y: y1_2})
+        y_pred = yi + h * y_prime.subs({x: xi, y: yi}).evalf()
+        yi = yi + h / 2 * (y_prime.subs({x: xi, y: yi}).evalf() + y_prime.subs({x: xi + h, y: y_pred}).evalf())
         xi += h
         result.append((xi, yi))
     
@@ -24,8 +23,8 @@ if __name__ == '__main__':
     y0 = 0
     h = 0.5
     f = 1 / 5 * x * sp.exp(3 * x) - 1/25 * sp.exp(3 * x) + 1 / 25 * sp.exp(-2 * x)
-    solution = mid_point(x0, y0, h, xn, y_prime, x, y)
-    print("Solution using Midpoint method:")
+    solution = heun(x0, y0, h, xn, y_prime, x, y)
+    print("Solution using Heun's method:")
     for x_val, y_val in solution:
         print(f"x: {x_val}, y: {y_val}", end='; ')
         print(f"Error: {abs(y_val - f.subs(x, x_val).evalf())}")
