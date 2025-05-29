ade0a5b51ddc8814c076d39efcd9b99b8498bc9a
phamducthien4325 <23020160@vnu.edu.vn>
2025-05-29 09:10:21 +0700

    Implement RK4 method for system of differential equations; add example usage and error calculation

---
+++ chuong7: phuong trinh vi phan/runge_kutta_sys.py
@@ -0,0 +1,46 @@
+import sympy as sp
+
+def rk4_system_method(x0: float, Y0: sp.Matrix, h: float, xn: float, F, x, Ysym) -> list[tuple[float, float]]:
+    xi = x0
+    Yi = Y0
+    result = [(xi, Yi)]
+
+    step = round((xn - x0) / h)
+    
+    for _ in range(int(step)):
+        subs1 = dict(zip(Ysym, Yi))
+        k1 = F.subs({x: xi}).applyfunc(lambda f: f.subs(subs1))
+
+        subs2 = dict(zip(Ysym, Yi + h/2 * k1))
+        k2 = F.subs({x: xi + h/2}).applyfunc(lambda f: f.subs(subs2))
+
+        subs3 = dict(zip(Ysym, Yi + h/2 * k2))
+        k3 = F.subs({x: xi + h/2}).applyfunc(lambda f: f.subs(subs3))
+
+        subs4 = dict(zip(Ysym, Yi + h * k3))
+        k4 = F.subs({x: xi + h}).applyfunc(lambda f: f.subs(subs4))
+        Yi = Yi + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+        xi += h
+        result.append((xi, Yi))
+    
+    return result
+
+if __name__ == '__main__':
+    x = sp.symbols('x')
+    ###########################################
+    Y = sp.Matrix(sp.symbols('y1 y2 y3')) 
+    F = sp.Matrix([Y[1] - Y[2] + x, 
+                   3 * x ** 2,
+                   Y[1] + sp.exp(-x)])
+    x0 = 0
+    xn = 1
+    h = 0.1
+    Y0 = sp.Matrix([1, 1, -1])
+    F = sp.Matrix([-0.05 * x ** 5 + 0.25 * x ** 4 + x + 2 - sp.exp(-x),
+                  x ** 3 + 1, 
+                  0.25 * x ** 4 + x - sp.exp(-x)])
+    ########################################
+    result = rk4_system_method(x0, Y0, h, xn, F, x, Y)
+    for xi, Yi in result:
+        print(f'x = {xi}, Y = {Yi}')
+        print(f"    Error: {abs(Yi - F.subs({x: xi}).evalf())}")
